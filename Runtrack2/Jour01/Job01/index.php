3503bcc2f37d11a7365304bf18c72c1c84265eb1
dylan-sauvat <121093882+dylan-sauvat@users.noreply.github.com>
2023-01-02 12:12:37 +0100

    Add files via upload

---
+++ Runtrack2/Jour01/Job01/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<htlm lang ="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    
+   </head>
+   <body>
+     <?php 
+       $str = "La Plateforme"; // affecte La Plateforme à la variable $str
+       $str2 = "Vive"; // affecte Vive à la variable $str2
+       $str3 = "!"; // affecte ! à la variable $str3
+
+     echo $str2. $str. $str3;
+     ?>
+     <br>
+     <?php
+     $val = "6"; // affecte 6 à la variable $val
+     
+     echo $val;
+    
+     $val = "$val + 4"; // ajoute 4 à la variable $val
+     
+     echo $val
+       ?>
+      <br>
+       <?php
+     $myBool = "true"; //affecte true à la variable $myBool
+     
+     echo $myBool;
+     $myBool = "false"; // affecte false à la variable $myBool
+     
+     echo $myBool;
+       ?>
+       
+      </body>
+   </html>
