a632bbf0e51d02a7d3bc61d377c78d55e67b66c5
dylan-sauvat <121093882+dylan-sauvat@users.noreply.github.com>
2023-01-04 11:50:41 +0100

    Add files via upload

--- Runtrack2/Jour03/Job06/index.php
+++ Runtrack2/Jour03/Job06/index.php
@@ -1,5 +1,5 @@
 <?php
-$str = "Les choses que l'on possède finissent par nous posséder.";
+$str = "Les choses que l'on possede finissent par nous posseder.";
 for ($i=strlen($str)-1; $i>=0; $i--) 
 {
     echo $str[$i];
