4513e5b1f9c26ba56ead1259327a05fbe52deee8
dylan-sauvat <121093882+dylan-sauvat@users.noreply.github.com>
2023-01-09 13:32:56 +0100

    Delete Runtrack2/Révision directory

--- Runtrack2/Révision/Jour02/Job02/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Voici la liste des nombres compris entre 1 et 1337 :";
-echo "<br/>";
-for ($i = 0; $i <= 1337; $i++) {
-    if ($i != 26 && $i != 37 && $i != 88 && $i != 1111 && $i != 3233) {
-        echo $i . '<br />';
-    }
-}
-?>
--- Runtrack2/Révision/Jour03/Job04/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$str = "Dans l'espace, personne ne vous entend crier";
-if (isset($str)) {
-    $length = strlen($str);
-    echo "Le nombre de caractères présents dans cette chaîne est de : " .$length;
-}
-
-?>
